59e2dbfefe132273f493723ce373711d442bd2d4
Nicole Sanchez <njbsanchez@gmail.com>
2022-06-10 01:50:27 -0700

    completed roadmap card inventory comparison & inventory report generator.

--- roadmap_card_inventory.py
+++ roadmap_card_inventory.py
@@ -5,100 +5,141 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+from datetime import date
 
 from bs4 import BeautifulSoup
 import json
 
+def get_page_source(url):
+    """
+    takes in url of page, returns page source
+    """
+    
+    options = Options()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    
+    driver.get(url)
+    more_buttons = driver.find_elements(By.CLASS_NAME, value="moreLink")
+    for x in range(len(more_buttons)):
+        if more_buttons[x].is_displayed():
+            driver.execute_script("arguments[0].click();", more_buttons[x])
+            time.sleep(1)
+        
+    page_source = driver.page_source
+    
+    return page_source
 
-options = Options()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
+def get_page_taxonomy(page_source):
+    """
+    takes in page source, returns content taxonomy
+    """
+    soup = BeautifulSoup(page_source, 'lxml')
+    content = soup.find_all(class_='inner')  
+    
+    return content
+     
+def create_card_dict(url):
+    """
+    takes in taxonomy, returns card_dictionary
+    """
+    
+    page_source = get_page_source(url)
+    content = get_page_taxonomy(page_source)
+    
+    cards_dictionary = {}
 
-driver = webdriver.Chrome("/Users/nsanchez/Desktop/tools_build/chromedriver", options=options)
+    for i,card in enumerate(content,start=1):
+    
+        title = str(card.h4.string)
+        # print('*************')
+        # print(title)
+        desc = 'N/A' if card.find(class_="description").p is None else str(((card.find(class_="description")).p.contents)[0])
+        try:
+            status = str(card.find(class_="custom-category").contents[0])
+        except:
+            category = str("missing")
+        try:
+            category = str(card.find(class_="custom-category2").contents[0])
+        except:
+            category = str("none")
+        try:
+            date = str(card.find(class_="custom-field-1").contents[0])
+        except:
+            date = str("none")
+        products = []
+        try:
+            for product in card.find(class_="custom-product").contents:
+                products.append(str(product.string))
+        except:
+            products = card.find(class_="custom-field-2").contents[0]
+        try:
+            for a in card.find_all('a', href=True):
+                link = str(a['href'])
+        except:
+            link = str('none')
 
+        cards_dictionary[i]={'title':title,
+                'description':desc,
+                'status':status,
+                'category':category,
+                'date':date,
+                'products':products,
+                'link':link
+                }
+    
+    return cards_dictionary
 
+def create_cloud_reports(cards_dictionary):
+    
+    today = date.today()
+    # print("Today's date:", today)
 
-# url = 'https://author.marketing.internal.atlassian.com/wac/roadmap/data-center?mgnlVersion=1.23'
+    file_name = f'cloud_cards_{today}'
+    with open(f'json_outputs/{file_name}.json', 'w') as outfile:
+        json.dump(cards_dictionary, outfile)
+    
+    y=json.dumps(cards_dictionary)
+    print(y)
+    
+    df = (pd.DataFrame.from_dict(cards_dictionary)).T
+    file = f'csv_outputs/{file_name}.csv'
+    df.to_csv (file, index = True, header=True)
+    
+def create_dc_reports(cards_dictionary):
+    
+    today = date.today()
+    # print("Today's date:", today)
 
-# driver.get(url)
-# more_buttons = driver.find_elements(By.CLASS_NAME, value="moreLink")
-# for x in range(len(more_buttons)):
-#   if more_buttons[x].is_displayed():
-#       driver.execute_script("arguments[0].click();", more_buttons[x])
-#       time.sleep(1)
-      
-# page_source = driver.page_source
-
-with open("v_12_16.html", "r", encoding='utf-8') as f:
-    text= f.read()
-
-page_source = text
-
-soup = BeautifulSoup(page_source, 'lxml')
-
+    file_name = f'dc_cards_{today}'
+    with open(f'json_outputs/{file_name}.json', 'w') as outfile:
+        json.dump(cards_dictionary, outfile)
+    
+    y=json.dumps(cards_dictionary)
+    print(y)
+    
+    df = (pd.DataFrame.from_dict(cards_dictionary)).T
+    file = f'csv_outputs/{file_name}.csv'
+    df.to_csv (file, index = True, header=True)
   
-content = soup.find_all(class_='inner')  
+def parse_cloud(cloud_url):
+    cloud_dictionary = create_card_dict(cloud_url)
+    create_cloud_reports(cloud_dictionary)
     
+def parse_dc(dc_url):
+    dc_dictionary = create_card_dict(dc_url)
+    create_dc_reports(dc_dictionary)
+
+
+if __name__ == "__main__":
     
-cards_dictionary = {}
+    # cloud_url = 'https://www.atlassian.com/roadmap/cloud'
+    dc_url = 'https://www.atlassian.com/roadmap/data-center'
 
-
-for i,card in enumerate(content,start=1):
-    
-    title = str(card.h4.string)
-    print('*************')
-    print(title)
-    desc = 'N/A' if card.find(class_="description").p is None else str(((card.find(class_="description")).p.contents)[0])
-    status = str(card.find(class_="custom-category").contents[0])
-    try:
-        category = str(card.find(class_="custom-category2").contents[0])
-    except:
-        category = str("none")
-    try:
-        date = str(card.find(class_="custom-field-1").contents[0])
-    except:
-        date = str("none")
-    products = []
-    try:
-        for product in card.find(class_="custom-product").contents:
-            products.append(str(product.string))
-    except:
-        products = card.find(class_="custom-field-2").contents[0]
-            
-
-    cards_dictionary[i]={'title':title,
-             'description':desc,
-             'status':status,
-             'category':category,
-             'date':date,
-             'products':products
-             }
-
-
-from datetime import date
-
-today = date.today()
-# print("Today's date:", today)
-
-file_name = f'cloud_cards_{today}'
-
-with open(f'json_outputs/{file_name}.json', 'w') as outfile:
-    json.dump(cards_dictionary, outfile)
-    
-    
-y=json.dumps(cards_dictionary)
-
-print(y)
-
-  
-
-
-df = (pd.DataFrame.from_dict(cards_dictionary)).T
-
-file = f'csv_outputs/{file_name}.csv'
-df.to_csv (file, index = True, header=True)
-    
+    # parse_cloud(cloud_url)
+    parse_dc(dc_url)
     
 
--- roadmap_compare.py
+++ roadmap_compare.py
@@ -1,9 +1,110 @@
 import pandas as pd
+import numpy as np
+import time
+from datetime import date
+import json
+import os
 
-df_1 = pd.read_csv("csv_outputs/dc_cards_2022-02-23.csv")
-df_2 = pd.read_csv("csv_outputs/dc_cards_2021-12-16.csv")
 
-# output the dataframe
+def get_all_statuses(old, new):
+    new_titles = (new['title'])
+    removed = old['title'].isin(new_titles)
+    all_changes = pd.merge(old, new, how='outer', on='title')
+    
+    return all_changes
 
-print(df_1.head(5))
-print(df_2.head(5))
+
+def create_report(df,roadmap,type):
+    
+    today = date.today()
+    # print("Today's date:", today)
+    
+    path = f'/Users/nsanchez/Desktop/card_parser/reports/{today}'
+
+    isExist = os.path.exists(path)
+
+    if not isExist:
+
+        os.makedirs(path)
+        print("The new directory is created!")
+
+    file_name = f'{roadmap}_report_{type}_{today}'
+    file = f'reports/{today}/{file_name}.csv'
+    df.to_csv(file, index=False, header=True)
+
+def get_updated_cards(all_changes):
+    
+    all_changes['changes'] = np.where(all_changes['status_y'].isnull(), "deleted", '')
+    all_changes['changes'] = np.where(all_changes['status_x'].isnull(), all_changes['changes'].astype(str) + 'net-new',all_changes['changes'].astype(str) + '' )
+
+    # seperate out updated cards
+
+    updated_cards = all_changes.loc[(all_changes['changes'] == '')]
+
+    updated_cards['desc_c'] = np.where(updated_cards['description_x'] != updated_cards['description_y'], 'description', None )
+    updated_cards['stat_c'] = np.where(updated_cards['status_x'] != updated_cards['status_y'], 'status', None )
+    updated_cards['cat_c'] = np.where(updated_cards['category_x'] != updated_cards['category_y'], 'category', None )
+    updated_cards['date_c'] = np.where(updated_cards['date_x'] != updated_cards['date_y'], 'date', None )
+    updated_cards['prod_c'] = np.where(updated_cards['products_x'] != updated_cards['products_y'], 'product(s)', None )
+    updated_cards['link_c'] = np.where(updated_cards['link_x'] != updated_cards['link_y'], 'link', None )
+
+    merged = updated_cards[['desc_c', 'stat_c', 'cat_c', 'date_c', 'prod_c', 'link_c']].values.tolist()
+    print("******************")
+    edit_merged = []
+    for item in merged:
+        item = set(item)
+        if len(item) > 1:
+            item.remove(None)
+        edit_merged.append(list(set(item)))
+        
+    print(edit_merged)
+    print("******************")
+    update_only = all_changes.loc[(all_changes['changes'] == '')]
+    update_only['changes'] = edit_merged
+    
+    return update_only
+
+def get_delete_only(all_changes):
+
+    delete_only = all_changes.loc[(all_changes['changes'] == 'deleted')]
+    
+    return delete_only
+
+def get_new_only(all_changes):
+
+    new_only = all_changes.loc[(all_changes['changes'] == 'net-new')]
+    
+    return new_only
+
+
+def compare_scrape(roadmap_type, old_csv, new_csv):
+    
+    old = pd.read_csv(old_csv)
+    new = pd.read_csv(new_csv)
+
+    # identify net-new, deleted, and updated cards
+
+    all_changes = get_all_statuses(old, new)
+
+    update_only = get_updated_cards(all_changes)
+
+    create_report(update_only,roadmap_type,"update")
+
+    new_only = get_new_only(all_changes)
+    
+    create_report(new_only,roadmap_type,"new")
+
+    delete_only = get_delete_only(all_changes)
+    
+    create_report(delete_only,roadmap_type,"delete")
+
+
+old_cloud_csv = "csv_outputs/cloud_cards_2022-06-08.csv"
+new_cloud_csv = "csv_outputs/cloud_cards_2022-06-09.csv"
+
+compare_scrape("cloud", old_cloud_csv, new_cloud_csv)
+
+old_dc_csv = "csv_outputs/dc_cards_2022-06-08.csv"
+new_dc_csv = "csv_outputs/dc_cards_2022-06-10.csv"
+
+compare_scrape("dc", old_dc_csv, new_dc_csv)
